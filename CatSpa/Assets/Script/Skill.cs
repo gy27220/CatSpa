6d13c8579d622be525b897858525c6e694cbd79f
GAYEON <gy27220@naver.com>
2022-09-06 00:38:55 +0900

    Cretate Skill Information
    스킬 정보 추가

--- CatSpa/Assets/Script/Skill.cs
+++ CatSpa/Assets/Script/Skill.cs
@@ -5,20 +5,13 @@ using UnityEngine.UI;
 
 public class Skill : MonoBehaviour
 {
+	//SkillInformation
+	//오브젝트가 갖는 기본적인 특징
 	#region public 변수
 	public Texture2D image;
 	public Text name; //string타입으로 바꿀지 고민. 일단 ㄱ
+	public string explanation; //각 스킬에 대한 설명
+	public int price; //가격
+	public float time; //서비스 시간
 	#endregion
-
-
-
-	void Start()
-	{
-
-	}
-
-	void Update()
-	{
-		
-	}
 }
