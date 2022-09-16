a0d4b28d22f44ee1e27fc83845d47c6b0cdce31b
GAYEON <gy27220@naver.com>
2022-09-16 17:11:57 +0900

    Add Ui
    ui 버튼을 통해 활성화/비활성화 기능을 구현함

--- CatSpa/Assets/01.Script/MouseClick.cs
+++ CatSpa/Assets/01.Script/MouseClick.cs
@@ -1,26 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class MouseClick : MonoBehaviour, IPointerClickHandler
-{
-	public void OnPointerClick(PointerEventData eventData)
+
+public class MouseClick : MonoBehaviour
+{ 
+	public interface Click
 	{
-		if(eventData.button == PointerEventData.InputButton.Left)
-		{
-			Debug.Log("Mouse Click Button : Left");
-		}
-		else if(eventData.button == PointerEventData.InputButton.Middle)
-		{
-			Debug.Log("Mouse Click Button : Middle");
-		}
-		else if(eventData.button == PointerEventData.InputButton.Right)
-		{
-			Debug.Log("Mouse Click Button : Right");
-		}
-
-		Debug.Log("Mouse Position :" + eventData.position);
-		Debug.Log("Mouse Click Count :" + eventData.clickCount);
+		void ButtonClick();
 	}
+
+	//public interface Off
+	//{
+	//	void DisableUi();
+	//}
 }
