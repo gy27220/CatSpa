72b7f36ee3d329012768cef60eb1f3cfa84a9942
GAYEON <gy27220@naver.com>
2022-09-22 13:07:00 +0900

    Amend UiInformation Script

--- CatSpa/Assets/01.Script/Uicatinformation.cs
+++ CatSpa/Assets/01.Script/Uicatinformation.cs
@@ -7,8 +7,6 @@ using TMPro;
 
 public class Uicatinformation : MouseClick
 {
-	//const int objectNumber = 1;
-
 	public GameObject catInformation;
 
 	[Header ("List")]
@@ -24,17 +22,6 @@ public class Uicatinformation : MouseClick
 	{
 		info_Image.sprite = list_Image.sprite;
 		info_Name.text = list_Name.text;
-
-
-		//버튼을 클릭했을 때 해당 버튼이 갖고있는 정보(자식들이 갖고있음)를 넘긴다.
-		//name = gameObject.GetComponentInChildren<TMPro.TextMeshProUGUI>().text;
-		//Image = gameObject.GetComponentsInChildren<Image>()[objectNumber].sprite;
-		//
-		//Debug.Log(catInformation.GetComponentInChildren<TMPro.TextMeshProUGUI>().text);
-
-		//catInformation.GetComponentsInChildren<Image>()[2].sprite = Image;
-		//catInformation.GetComponentsInChildren<TMPro.TextMeshProUGUI>()[0].text = name;
-		//catInformation.GetComponentsInChildren<TMPro.TextMeshProUGUI>()[1].text = produce;
 
 		catInformation.SetActive(true);
 	}
