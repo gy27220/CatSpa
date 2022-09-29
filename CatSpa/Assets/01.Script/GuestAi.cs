3e4298567e0177bca756ff88e65ab2fe429aae79
GAYEON <gy27220@naver.com>
2022-09-29 23:06:40 +0900

    Amend Guest
    1.guest오브젝트 라인업,레이어 정렬 수정.
    2.맨 앞 오브젝트 드래그시 뒤에 있는 오브젝트 내려오도록 수정.
    3.특정 애니메이션 안보였던 문제 수정.

--- CatSpa/Assets/01.Script/GuestAi.cs
+++ CatSpa/Assets/01.Script/GuestAi.cs
@@ -4,68 +4,63 @@ using UnityEngine;
 
 public class GuestAi : MonoBehaviour
 {
+	#region public 변수
+	public GameObject target;
 	public GameObject bubbleUi;
+	public float Speed = 2f;
+	#endregion
 
-	Vector2 pos;
-	float Speed = 2f;
+	//private
 	Animator ani;
+	SpriteRenderer spRender;
+	Vector2 pos;
+	bool isWalk;        //이동
+	bool isWaiting;     //대기
 
-	bool isWalk;
-
-	//
+	Drag isDrag;
 	//public GameObject selectOil;
-
 	//GameObject oil;
 
-	Vector2 objPos;
-	public Vector2 Position
-	{
-		set { objPos = value; }
-	}
-
 	void Start()
-    {
-		isWalk = true;
+	{
 		pos = transform.position;
-		ani = GetComponent<Animator>();
 
+		isWalk = true;
+		isWaiting = false;
+		ani = GetComponent<Animator>();
+		spRender = GetComponent<SpriteRenderer>();
+		isDrag = GetComponent<Drag>();
 		//oil = selectOil.GetComponent<RandomSelect>().Random_Select_Oil();
 	}
 
-    void Update()
-    {
-		//if (isWalk)
-		//Move(new Vector2(-1.5f, -1.4f));
-		// Move(new Vector2(objPos.x, objPos.y));
+	void Update()
+	{
+		//걷는 중 && 드래그중아님 && 기다리지도않음
+		if(isWalk && !isDrag.DragCheck && !isWaiting)
+			Move(target.transform.position);
 
-		//else
-		// massageEnd();
 
-		if(!isWalk)
-		{
-			ani.SetBool("Walk", true);
-			BubbleUi(true);
-		}
+		if(isDrag.DragCheck)
+			spRender.sortingOrder = 3;
 	}
 
-	void Move(Vector2 matPos)
+	void Move(Vector2 target)
 	{
-		pos = Vector2.MoveTowards(pos, matPos, Speed * Time.deltaTime);
-		transform.position = pos;
+		pos = Vector2.MoveTowards(pos, target, Speed * Time.deltaTime);
 
-		//목표 지점에 도착
-		if (matPos == pos)
-		{
-			ani.SetBool("Walk", true);
-			BubbleUi(true);
-		}
+		transform.position = pos;
 	}
 
 	void MassageEnd()
 	{
-		Move(new Vector2(0.95f, 3f));
 		BubbleUi(false);
 		ani.SetBool("Walk", false);
+	}
+
+	void Massage()
+	{
+		ani.SetBool("Walk", true);
+		BubbleUi(true);
 	}
 
 	void BubbleUi(bool setActive)
@@ -78,14 +73,25 @@ public class GuestAi : MonoBehaviour
 
 	void LayDown()
 	{
-		isWalk = false;
+		Massage();
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.gameObject.CompareTag("Mat"))
 		{
+			isWaiting = true;
 			Invoke("LayDown", 0.5f);
 		}
+
+		else if (collision.gameObject.tag == "Guest")
+			isWalk = false;
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Guest")
+			isWalk = true;
 	}
 }
+
